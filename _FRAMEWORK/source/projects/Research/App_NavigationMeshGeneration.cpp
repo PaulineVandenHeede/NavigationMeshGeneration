b9cfb149bbdf14f0b9301f758f9e2525333303f2
PaulineVandenHeede <vandenheede.pauline@gmail.com>
2022-08-05 08:46:35 +0200

    [MOD] - clean up main

--- _FRAMEWORK/source/projects/Research/App_NavigationMeshGeneration.cpp
+++ _FRAMEWORK/source/projects/Research/App_NavigationMeshGeneration.cpp
@@ -32,7 +32,9 @@ App_NavigationMeshGeneration::~App_NavigationMeshGeneration()
 
 void App_NavigationMeshGeneration::Start()
 {
-	//Elite::Polygon bounds = 
+	//Elite::Polygon bounds =
+	//-- TRY OUT: TRIANGULATION OF POLYGON --
+	{
 	/*Elite::Vertex v0{ Elite::Vector2{ 0.f, 0.f } };
 	Elite::Vertex v1{ Elite::Vector2{ 10.f, 7.f } };
 	Elite::Vertex v2{ Elite::Vector2{ 12.f, 3.f } };
@@ -58,46 +60,47 @@ void App_NavigationMeshGeneration::Start()
 	Elite::Vertex v3{ Elite::Vector2{ -10.f, -10.f } };
 	Elite::Vertex v4{ Elite::Vector2{ -10.f, 10.f } };*/
 
-	// -- TRY OUT: POLYGON WITH HOLE --
-	//Outside polygon
-	Elite::Vector2 v0 = Elite::Vector2{ 2.f, -3.f };
+	//NavMeshGeneration generator{};
+	//m_pNavMesh = generator.CreateNavMesh(*m_pStartPolygon, {});
+	}
+
+	// -- TRY OUT: EXPANDING POLYGON --
+	{
+	//-- Polygon 1: orthogonal
+	/*Elite::Vector2 v0 = Elite::Vector2{ 2.f, -3.f };
 	Elite::Vector2 v1 = Elite::Vector2{ 6.f, -3.f };
 	Elite::Vector2 v2 = Elite::Vector2{ 6.f, 1.f };
 	Elite::Vector2 v3 = Elite::Vector2{ 8.f, 1.f };
 	Elite::Vector2 v4 = Elite::Vector2{ 8.f, -3.f };
 	Elite::Vector2 v5 = Elite::Vector2{ 12.f, -3.f };
 	Elite::Vector2 v6 = Elite::Vector2{ 12.f, 4.f };
-	Elite::Vector2 v7 = Elite::Vector2{ 2.f, 4.f };
+	Elite::Vector2 v7 = Elite::Vector2{ 2.f, 4.f };*/
 
+	//-- Polygon 2: non-orthogonal
 	/*Elite::Vector2 v0 = Elite::Vector2{ 3.f, -2.f };
 	Elite::Vector2 v1 = Elite::Vector2{ 9.f, -2.f };
 	Elite::Vector2 v2 = Elite::Vector2{ 11.f, 3.f };
 	Elite::Vector2 v3 = Elite::Vector2{ 1.f, 3.f };*/
 
-
-
-	//Hole
-	Elite::Vector2 hv0 = Elite::Vector2{ 5.f, -1.f };
-	Elite::Vector2 hv1 = Elite::Vector2{ 5.f, -1.f };
-	Elite::Vector2 hv2 = Elite::Vector2{ 5.f, -1.f };
-	Elite::Vector2 hv3 = Elite::Vector2{ 5.f, -1.f };
-
-
-
-	m_pStartPolygon = new Geometry::Polygon{ { v0, v1, v2, v3, v4, v5, v6, v7 } };
+	/*m_pStartPolygon = new Geometry::Polygon{ { v0, v1, v2, v3, v4, v5, v6, v7 } };
 	m_pExpandedPolygon = new Geometry::Polygon{ {v0, v1, v2, v3, v4, v5, v6, v7 } };
 	m_pExpandedPolygon->Expand(2.f);
 
 	m_pStartPolygonElite = new Elite::Polygon{ std::list<Elite::Vector2>{ v0, v1, v2, v3, v4, v5, v6, v7 } };
 	m_pExpandedPolygonElite = new Elite::Polygon{ std::list<Elite::Vector2>{ v0, v1, v2, v3, v4, v5, v6, v7 } };
-	m_pExpandedPolygonElite->ExpandShape(-2.f);
+	m_pExpandedPolygonElite->ExpandShape(-2.f);*/
+	}
 
+
+	//-- TRY OUT: POLYGON WITH HOLE --
+	{
+	/*Elite::Vector2 hv0 = Elite::Vector2{ 5.f, -1.f };
+	Elite::Vector2 hv1 = Elite::Vector2{ 5.f, -1.f };
+	Elite::Vector2 hv2 = Elite::Vector2{ 5.f, -1.f };
+	Elite::Vector2 hv3 = Elite::Vector2{ 5.f, -1.f };*/
 
 	//m_pStartPolygon->AddChild(Geometry::Polygon{ hv0, hv1, hv2, hv3 });
-
-
-	//NavMeshGeneration generator{};
-	//m_pNavMesh = generator.CreateNavMesh(*m_pStartPolygon, {});
+	}
 }
 
 void App_NavigationMeshGeneration::Update(float deltaTime)
@@ -107,17 +110,22 @@ void App_NavigationMeshGeneration::Update(float deltaTime)
 
 void App_NavigationMeshGeneration::Render(float deltaTime) const
 {
-	//m_pStartPolygon->Draw();
-
-	size_t size = std::min(m_pStartPolygonElites.size(), m_pExpandedPolygonElites.size()); 
+	//-- TRY OUT: EXPANDING OF POLYGON
+	size_t size = min(m_pStartPolygonElites.size(), m_pExpandedPolygonElites.size()); 
 	for(size_t i = 0; i < size; ++i)
 	{
-		DEBUGRENDERER2D->DrawPolygon(m_pStartPolygonElites, Elite::Color::AquaMenthe);
-		DEBUGRENDERER2D->DrawPolygon(m_pExpandedPolygonElites, Elite::Color::Green);
+		DEBUGRENDERER2D->DrawPolygon(m_pStartPolygonElites[i], Elite::Color::AquaMenthe);
+		DEBUGRENDERER2D->DrawPolygon(m_pExpandedPolygonElites[i], Elite::Color::Green);
 	}
 
-	m_pExpandedPolygon->Draw();
+	size = min(m_pStartPolygons.size(), m_pExpandedPolygons.size());
+	for(size_t i = 0; i < size; ++i)
+	{
+		//m_pStartPolygons[i]->Draw();
+		m_pExpandedPolygons[i]->Draw();
+	}
 
+	//-- TRY OUT: TRIANGULATION OF POLYGON
 	//m_pNavMesh->DrawTriangles();
 	//m_pNavMesh->DrawCircumference();
 }
